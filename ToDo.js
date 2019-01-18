ffd30ee30a18f7b968dc3d3d1d6291f09a852712
Leigh Mathieson <leigh.mathieson@gmail.com>
2019-01-18 09:30:08 +0000

    add ToDo

---
+++ ToDo.js
@@ -0,0 +1,31 @@
+/*
+
+removeChild and destroy the reference to variables
+
+// 'Hey watch where you're going!'
+// 'Mirror, signal, manouvre!'
+// 'Just a scratch'
+// 'Oops, the engineers won't like this'
+// 'Note to self: use the paintballs'
+// // 'Watch the paint!'
+// 'That colour's so last year!'
+// 'Just doesn't suit me'
+// 'It's just been polished!'
+// 'The paint team won't be thrilled'
+// 'It's just been painted!'
+// 'It's just not me'
+// 'That will not do!'
+// 'Hmm polcadot coloured!'
+// 'There's just not enough multicoloured paint I feel'
+// 'Why I oughta..!'
+// 'How rude!'
+// 'Note to self: close the mouth next time!'
+
+balloon with unlimited bullets on screen for 10 secs
+
+Need to do a sweep after every plane restart for   gameScene.children._texture.textureCacheIds[0]
+ if they contain  RBPaintBall  or rafPaintball.  If so remove them
+
+ Paint splats on the floor? /  Swap painted hut image in
+
+*/
